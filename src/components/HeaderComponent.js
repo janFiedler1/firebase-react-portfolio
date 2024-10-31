3fab7e4fa3542b84424e5e5c13d631338c8e49a8
janFiedler1 <50682895+janFiedler1@users.noreply.github.com>
2024-10-31 15:36:30 -0400

    work on homepage

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,27 +1,54 @@
-import {useState, Fragment} from "react";
+import {useState, Fragment, useEffect} from "react";
 import '../css/HeaderComponent.css'
 import { useParams } from "react-router-dom";
+import 'http://code.jquery.com/jquery-latest.min.js';
 
 export default function Header({navLinks, changeContent, router}) {
 
     const [displayNone, setDisplay] = useState("");
     const display = () => {
         console.log("click works: "+display == "" ? " display-none" : "");
-        setDisplay(displayNone == "" ? " display-none" : "")
+        // setDisplay(displayNone == "" ? " display-none" : "")
+        setDisplay(" display-none");
         return displayNone;
     }
 
     const [translation, translate] = useState("");
     const move = () => {
-        translate(translation == "" ? " translate" : "")
+        // translate(translation == "" ? " translate" : "")
+        translate(" translate");
+        window.scroll({
+            top: 100,
+            left: 0,
+            behavior: "smooth",
+          });
         return translation;
+    }
+
+
+    const scroll = () => {
+        //move();
+        setTimeout(function(){
+            display();
+        }, 2000);
+    }
+
+    window.onscroll = function() {myFunction()};
+    const [color, setColor] = useState("");
+
+    function myFunction() {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            setColor(" white");
+        } else {
+            setColor("");
+        }
     }
 
     return (
         <Fragment>
-            <span onClick={display}>disappear</span>
-            <span onClick={move}>move</span>
-            <div id="header" className={"header"+displayNone+translation}>
+            {/* <span onClick={display}>disappear</span>
+            <span onClick={move}>move</span> */}
+            <div id="header" className={"header"+displayNone+translation+color}>
                 <a href="/">
                     <div className="logo-container">
                         <span>JF</span>
--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -3,6 +3,9 @@ import '../css/HomeComponent.css'
 
 export default function Home() {
 
+    const skills = ["ReactJS", "NodeJS", "Symfony", "Java", "Python", "C#", "Git", "Jira", "SQL", "DBeaver", "Datadog", "Unity", "AWS", "Linux"];
+    const interests = ["Running", "Hiking", "Chess"];
+
     const socialLinks = [
         {id:1, name: 'linkedin', url: 'https://www.linkedin.com/in/jfiedleruwo/', img: process.env.PUBLIC_URL+"/linkedin2.png"},
         {id:2, name: 'github', url: 'https://github.com/janFiedler1', img: process.env.PUBLIC_URL+"/github2.png"},
@@ -36,10 +39,32 @@ export default function Home() {
                         ))}
                     </div>
                 </div>
-                <div className="intro-container"></div>
+                <div className="intro-container info-container">
+                    <h2>About Me</h2>
+                    <p>Hey, I'm Jan! Welcome to my webpage.</p>
+                    <p>I'm a passionate and skilled Full-Stack Developer with a bachelor's degree in Computer Science. With over two years of experience in Symfony, I've honed my abilities in building robust and scalable web applications. My expertise extends to React, enabling me to craft dynamic and user-friendly front-end experiences.</p>
+                    <p>I enjoy thinking and questioning. It's how you get to the root to really learn and understand things in all areas of life. “The more I learn, the less I realize I know”. I'm also a Christian, and in the area of work, I believe that work was designed as a good thing, both for others and ourselves. I'm a self-taught software developer living in Windsor, Ontario, Canada and a big fan of all-things javascript (although I do know it's weird at times...) I love computer science because of the ability you gain to be able to see an idea (applicable to any field) through, from start to finish. Check out some of my work and experience.</p>
+                    <p>I'm also passionate about music and other ideas 😃</p>
+                </div>
                 {/* </div> */}
-                <div className="skills-container"></div>
-                <div className="interests-container"></div>
+                <div className="row">
+                    <div className="skills-container info-container">
+                        <h2>Skills</h2>
+                        <div className="skill-container">
+                            {skills.map((skill) => (
+                                <span>{skill}</span>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="interests-container info-container">
+                        <h2>Interests</h2>
+                        <div className="skill-container">
+                            {interests.map((skill) => (
+                                <span>{skill}</span>
+                            ))}
+                        </div>
+                    </div>
+                </div>
             </div>
         </React.Fragment>
     )
