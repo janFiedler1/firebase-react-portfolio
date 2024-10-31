d9dbde1e441fee96e929db9413f486f248963cf4
janFiedler1 <50682895+janFiedler1@users.noreply.github.com>
2024-10-31 16:43:51 -0400

    copy

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,54 +1,55 @@
 import {useState, Fragment, useEffect} from "react";
 import '../css/HeaderComponent.css'
 import { useParams } from "react-router-dom";
-import 'http://code.jquery.com/jquery-latest.min.js';
+// import 'http://code.jquery.com/jquery-latest.min.js';
 
 export default function Header({navLinks, changeContent, router}) {
 
-    const [displayNone, setDisplay] = useState("");
-    const display = () => {
-        console.log("click works: "+display == "" ? " display-none" : "");
-        // setDisplay(displayNone == "" ? " display-none" : "")
-        setDisplay(" display-none");
-        return displayNone;
-    }
+    // const [displayNone, setDisplay] = useState("");
+    // const display = () => {
+    //     console.log("click works: "+display == "" ? " display-none" : "");
+    //     // setDisplay(displayNone == "" ? " display-none" : "")
+    //     setDisplay(" display-none");
+    //     return displayNone;
+    // }
 
-    const [translation, translate] = useState("");
-    const move = () => {
-        // translate(translation == "" ? " translate" : "")
-        translate(" translate");
-        window.scroll({
-            top: 100,
-            left: 0,
-            behavior: "smooth",
-          });
-        return translation;
-    }
+    // const [translation, translate] = useState("");
+    // const move = () => {
+    //     // translate(translation == "" ? " translate" : "")
+    //     translate(" translate");
+    //     window.scroll({
+    //         top: 100,
+    //         left: 0,
+    //         behavior: "smooth",
+    //       });
+    //     return translation;
+    // }
 
 
-    const scroll = () => {
-        //move();
-        setTimeout(function(){
-            display();
-        }, 2000);
-    }
+    // const scroll = () => {
+    //     //move();
+    //     setTimeout(function(){
+    //         display();
+    //     }, 2000);
+    // }
 
-    window.onscroll = function() {myFunction()};
-    const [color, setColor] = useState("");
+    // window.onscroll = function() {myFunction()};
+    // const [color, setColor] = useState("");
 
-    function myFunction() {
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            setColor(" white");
-        } else {
-            setColor("");
-        }
-    }
+    // function myFunction() {
+    //     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    //         setColor(" white");
+    //     } else {
+    //         setColor("");
+    //     }
+    // }
 
     return (
         <Fragment>
             {/* <span onClick={display}>disappear</span>
             <span onClick={move}>move</span> */}
-            <div id="header" className={"header"+displayNone+translation+color}>
+            {/* <div id="header" className={"header"+displayNone+translation+color}> */}
+            <div id="header" className={"header"}>
                 <a href="/">
                     <div className="logo-container">
                         <span>JF</span>
