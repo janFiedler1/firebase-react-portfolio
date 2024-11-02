6d01f88ed33e5805be969fd60e3ec83c2f27910a
janFiedler1 <50682895+janFiedler1@users.noreply.github.com>
2024-11-01 23:54:41 -0400

    copy

--- src/components/PortfolioComponent.js
+++ src/components/PortfolioComponent.js
@@ -3,14 +3,7 @@ import '../css/PortfolioComponent.css'
 
 const projects = [
     {id: 1, name: "Natural Instinct", url: 'https://naturalinstinct.shop', img: process.env.PUBLIC_URL+'/naturalinstinct.shop_.png', description: "Natural instinct is a static e-commerce web page using ReactJS and hosted on Firebase"},
-    {id: 1, name: "Chess JS", url: 'https://janfiedler1.github.io/chess-js/', img: process.env.PUBLIC_URL+'/chess.png', description: "Chess JS is a playable chess board written in vanila Javascript"},
-    {id: 1, name: "Natural Instinct", url: 'https://naturalinstinct.shop', img: process.env.PUBLIC_URL+'/naturalinstinct.shop_.png', description: "Natural instinct is a static e-commerce web page using ReactJS and hosted on Firebase"},
-    {id: 1, name: "Natural Instinct", url: 'https://naturalinstinct.shop', img: process.env.PUBLIC_URL+'/naturalinstinct.shop_.png', description: "Natural instinct is a static e-commerce web page using ReactJS and hosted on Firebase"},
-    {id: 1, name: "Natural Instinct", url: 'https://naturalinstinct.shop', img: process.env.PUBLIC_URL+'/naturalinstinct.shop_.png', description: "Natural instinct is a static e-commerce web page using ReactJS and hosted on Firebase"},
-    {id: 1, name: "Natural Instinct", url: 'https://naturalinstinct.shop', img: process.env.PUBLIC_URL+'/naturalinstinct.shop_.png', description: "Natural instinct is a static e-commerce web page using ReactJS and hosted on Firebase"},
-    {id: 1, name: "Natural Instinct", url: 'https://naturalinstinct.shop', img: process.env.PUBLIC_URL+'/naturalinstinct.shop_.png', description: "Natural instinct is a static e-commerce web page using ReactJS and hosted on Firebase"},
-    {id: 1, name: "Natural Instinct", url: 'https://naturalinstinct.shop', img: process.env.PUBLIC_URL+'/naturalinstinct.shop_.png', description: "Natural instinct is a static e-commerce web page using ReactJS and hosted on Firebase"},
-    {id: 1, name: "Natural Instinct", url: 'https://naturalinstinct.shop', img: process.env.PUBLIC_URL+'/naturalinstinct.shop_.png', description: "Natural instinct is a static e-commerce web page using ReactJS and hosted on Firebase"},
+    {id: 2, name: "Chess JS", url: 'https://janfiedler1.github.io/chess-js/', img: process.env.PUBLIC_URL+'/chess.png', description: "Chess JS is a playable chess board written in vanila Javascript"},
 ]
 
 export default function Portfolio() {
@@ -18,7 +11,7 @@ export default function Portfolio() {
         <React.Fragment>
             <div className="portfolio-container">
                 <h2>Portfolio</h2>
-                <span className="portfolio-description">Expand a project to view more details</span>
+                <span className="portfolio-description">Click on a project to see live view</span>
                 <div className="projects-container">
                     {projects.map((project) => (
                         <a href={project.url} target="_blank">
